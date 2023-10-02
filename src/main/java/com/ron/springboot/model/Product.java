d6048ed98f24fac49d773f4deb25073093d570ca
znwang <syy87626@gmail.com>
2023-10-03 05:28:07 +0800

    初始化Springboot Project

--- src/main/java/com/ron/springboot/model/Product.java
+++ src/main/java/com/ron/springboot/model/Product.java
@@ -5,6 +5,11 @@ public class Product {
     private Integer productId;
     private String productName;
     private String category;
-    private String imageurl;
+    private String imageUrl;
+    private Integer price;
+    private Integer stock;
+    private String description;
+    private Date createDate;
+
 
 }
